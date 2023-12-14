4aaef36ef55abc08eeb86051b18fe6442dff962b
suhaib927 <liosu927@gmail.com>
2023-12-14 20:38:25 +0300

    .

---
+++ Hospital-appointment-system/Migrations/20231214173515_AddCategoryToDatabase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital_appointment_system.Migrations
+{
+    public partial class AddCategoryToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
