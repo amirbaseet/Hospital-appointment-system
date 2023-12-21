765ff3caa8f1dc57d019a1ebf7dad7dc44d9d963
amirbaseet <amro.baseet@ogr.sakarya.edu.tr>
2023-12-21 15:41:52 +0300

    finished

--- Hospital-appointment-system/Controllers/PatientUserController.cs
+++ Hospital-appointment-system/Controllers/PatientUserController.cs
@@ -146,50 +146,7 @@ namespace Hospital_appointment_system.Controllers
             return View(patientUser);
         }
 
-        [HttpGet]
-        [Authorize(Roles = UserRoles.Admin)]
-
-        public IActionResult CreateAdmin()
-        {
-            @ViewData["Create"] = _localizer["Create"];
-            @ViewData["BacktoList"] = _localizer["BacktoList"];
-            return View();
-        }
-        [Authorize(Roles = UserRoles.Admin)]
-        [HttpPost]
-        public async Task<IActionResult> CreateAdmin(RegisterViewModel patientUser)
-        {
-         
-            if (ModelState.IsValid)
-            {
-
-                if (await _context.PatientUsers.AnyAsync(u => u.Email.Equals(patientUser.EmailAddress)))
-                {
-                    TempData["Error"] = "This email address is already in use";
-                    return View(patientUser);
-                }
-                var user = new PatientUser
-                {
-                    UserName = patientUser.Username,
-                    Email = patientUser.EmailAddress,
-                    Gender = patientUser.Gender,
-                    EmailConfirmed = true  // or set based on your application logic
-                };
-
-                var result = await _userManager.CreateAsync(user, patientUser.Password);
-
-                // Optionally add user to a role
-                if (result.Succeeded)
-                {
-                    await _userManager.AddToRoleAsync(user, UserRoles.Admin);  // Assign a default role or based on model
-                }
-
-                return RedirectToAction(nameof(ListAdmin));
-            }
-            TempData["Error"] = "entered information is not correct";
-            // If we reach here, something went wrong, re-show form
-            return View(patientUser);
-        }
+       
         [Authorize(Roles = UserRoles.Admin)]
         //GET Edit
         [HttpGet]
@@ -286,7 +243,7 @@ namespace Hospital_appointment_system.Controllers
         [HttpPost]
         public async Task<IActionResult> Delete(PatientUser User)
         {
-           
+
             var isAdmin = await _userManager.IsInRoleAsync(User, "Admin");
 
             var user1 = await _userManager.FindByIdAsync(User.Id);
@@ -300,6 +257,159 @@ namespace Hospital_appointment_system.Controllers
             else
                 return RedirectToAction(nameof(ListPatients));
         }
-    }
+        ///////////////////////////////////////////////////////////////
+        ///Admin part
+        [Authorize(Roles = UserRoles.Admin)]
+        //GET Edit
+        [HttpGet]
+
+        public async Task<IActionResult> EditAdmin(string? id)
+        {
+            @ViewData["EditPatient"] = _localizer["EditPatient"];
+            @ViewData["Update"] = _localizer["Update"];
+            @ViewData["BacktoList"] = _localizer["BacktoList"];
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patientUser = await _context.PatientUsers.FindAsync(id);
+            if (patientUser == null)
+            {
+                return NotFound();
+            }
+
+            // Pass the PatientUser model to the view
+            return View(patientUser);
+        }
+        //POST Edit
+        //[Authorize(Roles = UserRoles.Admin)]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditAdmin(PatientUser model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.FindByIdAsync(model.Id);
+            if (user == null)
+            {
+                // Handle the case where the user isn't found
+                return NotFound();
+            }
+            //getting the user role
+            var isAdmin = await _userManager.IsInRoleAsync(user, "Admin");
+            // Update the user's properties
+            user.UserName = model.UserName;
+            user.Email = model.Email;
+            user.Gender = model.Gender;
+
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+
+                //checking the user`s role to redirect to its own list page
+                if (isAdmin)
+                {
+                    return RedirectToAction(nameof(ListAdmin));
+                }
+                else
+                    return RedirectToAction(nameof(ListPatients));
+            }
+            else
+            {
+                // Handle errors
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
+        }
+        [Authorize(Roles = UserRoles.Admin)]
+
+        // GET: User/Delete
+        [HttpGet]
+        public async Task<IActionResult> DeleteAdmin(string? id)
+        {
+            @ViewData["BacktoList"] = _localizer["BacktoList"];
+            @ViewData["DeletePatient"] = _localizer["DeletePatient"];
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var patientsFromDb = await _context.PatientUsers.FindAsync(id);
+            if (patientsFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(patientsFromDb);
+
+        }
+        public async Task<IActionResult> DeleteAdmin(PatientUser User)
+        {
+
+            var isAdmin = await _userManager.IsInRoleAsync(User, "Admin");
+
+            var user1 = await _userManager.FindByIdAsync(User.Id);
+            _context.Remove(user1);
+            var result = _context.SaveChanges();
+            //checking the user`s role to redirect to its own list page
+            if (isAdmin)
+            {
+                return RedirectToAction(nameof(ListAdmin));
+            }
+            else
+                return RedirectToAction(nameof(ListPatients));
+        }
+
+        [HttpGet]
+        [Authorize(Roles = UserRoles.Admin)]
+
+        public IActionResult CreateAdmin()
+        {
+            @ViewData["Create"] = _localizer["Create"];
+            @ViewData["BacktoList"] = _localizer["BacktoList"];
+            return View();
+        }
+        [Authorize(Roles = UserRoles.Admin)]
+        [HttpPost]
+        public async Task<IActionResult> CreateAdmin(RegisterViewModel patientUser)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                if (await _context.PatientUsers.AnyAsync(u => u.Email.Equals(patientUser.EmailAddress)))
+                {
+                    TempData["Error"] = "This email address is already in use";
+                    return View(patientUser);
+                }
+                var user = new PatientUser
+                {
+                    UserName = patientUser.Username,
+                    Email = patientUser.EmailAddress,
+                    Gender = patientUser.Gender,
+                    EmailConfirmed = true  // or set based on your application logic
+                };
+
+                var result = await _userManager.CreateAsync(user, patientUser.Password);
+
+                // Optionally add user to a role
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, UserRoles.Admin);  // Assign a default role or based on model
+                }
+
+                return RedirectToAction(nameof(ListAdmin));
+            }
+            TempData["Error"] = "entered information is not correct";
+            // If we reach here, something went wrong, re-show form
+            return View(patientUser);
+        }    }
 }
 
